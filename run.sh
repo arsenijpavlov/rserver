b9e10a5608a24165a806629159d31a41d622b7b6
Арсений Павлов <43166722+arsenijpavlov@users.noreply.github.com>
2024-05-08 16:19:36 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+wget ifconfig.ru
+if [[ $? != 0 ]]; then
+  echo "wget return $?";
+  exit;
+fi
+
+read new_ip < index.html
+if [[ $new_ip && $new_ip != $last_ip ]]; then
+  echo "[RSERVER]($new_ip)" > README.md;
+  git add README.md;
+  git commit -m "Upd IP";
+  git push;
+fi
