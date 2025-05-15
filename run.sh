6f96c6085f4866e539d2e50ba703c545ab31e0d3
Riddick <arsenijpavlov@gmail.com>
2025-05-15 12:35:30 +0300

    upd script for usage ifconfig.com

--- run.sh
+++ run.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-wget ifconfig.ru
+echo "wget ifconfig"
+rm -f index.html
+wget ifconfig.com #.ru перестал работать...
 if [[ $? != 0 ]]; then
   echo "wget return $?";
   exit;
 fi
 
-read last_ip < last_IP.txt;
-read new_ip < index.html;
-if [[ $new_ip && $new_ip != $last_ip ]]; then
+read last_ip < last_IP.txt
+read new_ip < index.html
+echo "last_ip: ${last_ip}"
+echo "new_ip: ${new_ip}"
+#echo $(date) >> wrote.txt
+#if [[ $new_ip && $new_ip != $last_ip ]]; then
   echo $new_ip > last_IP.txt;
-  echo $(date) >> wrote.txt
+  #echo $(date) >> wrote.txt
   echo "[RSERVER](http://$new_ip)" > README.md;
   git add README.md;
   git commit -m "Upd IP";
   git push;
-fi
+#else
+#  echo "nothing update";
+#fi
 
 rm index.html*;
